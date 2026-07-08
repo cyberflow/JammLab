bcbd9ebe8adc5c4640c31c6bd4a1b01d4c0a7459
Cyberflow <dmitry.r@cyberflow.net>
2026-07-08 17:15:16 +0300

    refactor: split project save destination tests

--- JammLabTests/TimelineProjectLogicTests.swift
+++ JammLabTests/TimelineProjectLogicTests.swift
@@ -2,45 +2,6 @@ import XCTest
 @testable import JammLab
 
 final class TimelineProjectLogicTests: XCTestCase {
-    func testProjectSaveDestinationCreatesProjectSubdirectory() {
-        let selectedURL = URL(fileURLWithPath: "/tmp/JammLab/Song")
-        let destination = ProjectSaveDestination.projectFolder(selectedURL)
-
-        XCTAssertEqual(destination.artifactRootURL.path, "/tmp/JammLab/Song")
-        XCTAssertEqual(destination.projectURL.path, "/tmp/JammLab/Song/Song.jammlab")
-        XCTAssertEqual(destination.securityScopedAccessURL.path, "/tmp/JammLab/Song")
-        XCTAssertTrue(destination.createSubdirectory)
-    }
-
-    func testProjectSaveDestinationWithoutSubdirectoryUsesSelectedProjectFile() {
-        let selectedURL = URL(fileURLWithPath: "/tmp/JammLab/Song.jammlab")
-        let destination = ProjectSaveDestination.projectFile(selectedURL)
-
-        XCTAssertEqual(destination.artifactRootURL.path, "/tmp/JammLab")
-        XCTAssertEqual(destination.projectURL.path, "/tmp/JammLab/Song.jammlab")
-        XCTAssertEqual(destination.securityScopedAccessURL.path, "/tmp/JammLab")
-        XCTAssertFalse(destination.createSubdirectory)
-    }
-
-    func testProjectSaveDestinationStripsJammlabExtensionFromProjectFolderSelection() {
-        let selectedURL = URL(fileURLWithPath: "/tmp/JammLab/Song.jammlab")
-        let destination = ProjectSaveDestination.projectFolder(selectedURL)
-
-        XCTAssertEqual(destination.artifactRootURL.path, "/tmp/JammLab/Song")
-        XCTAssertEqual(destination.projectURL.path, "/tmp/JammLab/Song/Song.jammlab")
-        XCTAssertEqual(destination.securityScopedAccessURL.path, "/tmp/JammLab/Song")
-    }
-
-    func testProjectSaveDestinationAddsJammlabExtensionForFileMode() {
-        let selectedURL = URL(fileURLWithPath: "/tmp/JammLab/Song")
-        let destination = ProjectSaveDestination.projectFile(selectedURL)
-
-        XCTAssertEqual(destination.artifactRootURL.path, "/tmp/JammLab")
-        XCTAssertEqual(destination.projectURL.path, "/tmp/JammLab/Song.jammlab")
-        XCTAssertEqual(destination.securityScopedAccessURL.path, "/tmp/JammLab")
-        XCTAssertFalse(destination.createSubdirectory)
-    }
-
     func testLoopRegionRespectsCustomMinimumLength() {
         let region = LoopRegion(start: 2, end: 5)
 
