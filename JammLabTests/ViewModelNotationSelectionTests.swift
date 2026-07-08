e4ac97b8802b05f839d8bc07696c3c16b7794b5c
Cyberflow <dmitry.r@cyberflow.net>
2026-07-08 17:47:23 +0300

    refactor: split notation duration selection tests

--- JammLabTests/ViewModelNotationSelectionTests.swift
+++ JammLabTests/ViewModelNotationSelectionTests.swift
@@ -88,42 +88,6 @@ final class ViewModelNotationSelectionTests: XCTestCase {
     }
 
     @MainActor
-    func testChangingSelectedWholeRestToQuarterCreatesTwoQuartersAndHalfInFourFour() throws {
-        let viewModel = try loadedNotationViewModel(duration: 8)
-        let measure = try notationMeasure(1, in: viewModel)
-        let item = try XCTUnwrap(measure.notationItems.first)
-        viewModel.markProjectClean()
-
-        viewModel.selectNotationItem(NotationItemSelection(measure: measure, item: item))
-        viewModel.setNotationDurationDenominator(4)
-
-        let updatedMeasure = try notationMeasure(1, in: viewModel)
-        XCTAssertEqual(updatedMeasure.notationItems.map(\.displayDuration.denominator), [4, 4, 2])
-        XCTAssertEqual(updatedMeasure.notationItems.map(\.offsetInQuarterNotes), [0, 1, 2])
-        XCTAssertEqual(updatedMeasure.notationItems.map(\.durationInQuarterNotes), [1, 1, 2])
-        XCTAssertEqual(viewModel.selectedNotationItem?.offsetInQuarterNotes, 0)
-        XCTAssertTrue(viewModel.isProjectModified)
-    }
-
-    @MainActor
-    func testChangingSelectedWholeRestToHalfInThreeFourCreatesHalfAndQuarter() throws {
-        let viewModel = try loadedNotationViewModel(duration: 6)
-        viewModel.setTimeSignature(beatsPerBar: 3, beatUnit: 4)
-        let measure = try notationMeasure(1, in: viewModel)
-        let item = try XCTUnwrap(measure.notationItems.first)
-        viewModel.markProjectClean()
-
-        viewModel.selectNotationItem(NotationItemSelection(measure: measure, item: item))
-        viewModel.setNotationDurationDenominator(2)
-
-        let updatedMeasure = try notationMeasure(1, in: viewModel)
-        XCTAssertEqual(updatedMeasure.notationItems.map(\.displayDuration.denominator), [2, 4])
-        XCTAssertEqual(updatedMeasure.notationItems.map(\.offsetInQuarterNotes), [0, 2])
-        XCTAssertEqual(updatedMeasure.notationItems.map(\.durationInQuarterNotes), [2, 1])
-        XCTAssertTrue(viewModel.isProjectModified)
-    }
-
-    @MainActor
     func testShiftSelectingNotationMeasuresBuildsContiguousRange() throws {
         let viewModel = try loadedNotationViewModel(duration: 8)
         let firstMeasure = try notationMeasure(1, in: viewModel)
