37308d2e536ac8a037e66be9706b0584373d6daf
Cyberflow <dmitry.r@cyberflow.net>
2026-07-08 18:13:09 +0300

    refactor: share SHA256 hex formatting

--- JammLab/Services/StemSeparationService.swift
+++ JammLab/Services/StemSeparationService.swift
@@ -508,7 +508,7 @@ final class StemSeparationService {
         ].joined(separator: "|")
 
         let digest = SHA256.hash(data: Data(rawValue.utf8))
-        return digest.map { String(format: "%02x", $0) }.joined()
+        return digest.lowercaseHexString
     }
 
     private func applicationSupportDirectory() -> URL {
